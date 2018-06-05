de3f321005b6c7499bcc6c9ccf8d6eb4332010b8
Yongcong Ostby <yongcong5@yahoo.com>
2018-06-05 05:39:34 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(session(
 
 app.set('port', 4200);
 
-var prod_addr = "https://desolate-gorge-63477.herokuapp.com/";
+var prod_addr = "https://steamcheck.herokuapp.com/";
 
 var dev_addr = "http://localhost:4200";
 
